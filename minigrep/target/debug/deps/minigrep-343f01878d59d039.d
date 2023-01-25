03a572ddf221add8e453fc56b54e6f05c27464c0
hyunho lee <sailor3947@gmail.com>
2023-01-25 14:38:10 +0900

    add: env var

---
+++ minigrep/target/debug/deps/minigrep-343f01878d59d039.d
@@ -0,0 +1,5 @@
+/home/chiefmate/sideprojects/minigrep/target/debug/deps/minigrep-343f01878d59d039: src/main.rs
+
+/home/chiefmate/sideprojects/minigrep/target/debug/deps/minigrep-343f01878d59d039.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-600e44c7cac97d54.d
@@ -0,0 +1,5 @@
+/home/chiefmate/sideprojects/minigrep/target/debug/deps/minigrep-600e44c7cac97d54: src/lib.rs
+
+/home/chiefmate/sideprojects/minigrep/target/debug/deps/minigrep-600e44c7cac97d54.d: src/lib.rs
+
+src/lib.rs:

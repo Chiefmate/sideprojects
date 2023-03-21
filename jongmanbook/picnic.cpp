bca9e543e6258a02a6450ffa6a152cd4f927f634
Hyunho Lee <hyunho@Hyunhoui-MacBookPro.local>
2023-03-22 00:52:27 +0900

    picnic not done

---
+++ jongmanbook/picnic.cpp
@@ -0,0 +1,28 @@
+// picnic
+// 22 Mar 2023
+
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+const int MAX = 200007;
+const int MOD = 1000000007;
+ 
+void solve() {
+	int n, m, input;
+    vector<int> friends;
+	cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> input;
+        friends.push_back(input);
+    }
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int tt; 
+    cin >> tt; 
+    for (int i = 0; i < tt; i++) {solve();}
+}
